62d7eb18bd592bb0da620f06d5a235acf66e1b36
Delcio Monjane <1211739@isep.ipp.pt>
2023-10-28 14:54:35 +0100

    FRG-20 controller + service integration test

--- tests/robotController.test.ts
+++ tests/robotController.test.ts
@@ -12,6 +12,11 @@ import { RobotTypeBrand } from "../src/domain/robotTypeBrand";
 import { RobotType } from "../src/domain/robotType";
 import { UniqueEntityID } from "../src/core/domain/UniqueEntityID";
 import { IRobotTypeDTO } from "../src/dto/IRobotTypeDTO";
+import { Robot } from "../src/domain/robot";
+import { RobotNickName } from "../src/domain/robotNickName";
+import { RobotCode } from "../src/domain/robotCode";
+import { RobotSerialNr } from "../src/domain/robotSerialNr";
+import { RobotDescription } from "../src/domain/robotDescription";
 
 describe("robot controller", function() {
   const sandbox = sinon.createSandbox();
@@ -65,7 +70,6 @@ describe("robot controller", function() {
     };
 
 
-
     let robotServiceInstance = Container.get("RobotService");
     sinon.stub(robotServiceInstance, "createRobot").returns(Result.ok<IRobotDTO>({
       "nickName": req.body.nickName,
@@ -92,4 +96,92 @@ describe("robot controller", function() {
       "state": "true"
     }));
   });
+
+  it("robotController + robotService unit test (create) using robot, robotTypeRepo and robotRepo stubs ", async function() {
+    // Arrange
+    let body = {
+      "nickName": "Son",
+      "robotCode": "FDR46",
+      "serialNr": "AS35",
+      "description": "ready to work",
+      "robotType": "TipoA"
+    };
+    let req: Partial<Request> = {};
+    req.body = body;
+
+    let res: Partial<Response> = {
+      json: sinon.spy()
+    };
+    let next: Partial<NextFunction> = () => {
+
+    };
+
+    sinon.stub(Robot, "create").returns(Result.ok(
+      {
+        "id": "124",
+        "nickName": RobotNickName.create(req.body.nickName).getValue(),
+        "robotCode": RobotCode.create(req.body.robotCode).getValue(),
+        "serialNr": RobotSerialNr.create(req.body.serialNr).getValue(),
+        "description": RobotDescription.create(req.body.description).getValue(),
+        "state": true,
+        "robotType": RobotType.create({
+            "name": RobotTypeName.create(req.body.robotType).getValue(),
+            "taskTypes": [TaskType.delivery],
+            "robotTypeModel": RobotTypeModel.create("RB13").getValue(),
+            "brand": RobotTypeBrand.create("RB").getValue(),
+          }, new UniqueEntityID("123")).getValue()
+      }
+    ));
+
+    let robotTypeRepoInstance = Container.get("RobotTypeRepo");
+    sinon.stub(robotTypeRepoInstance, "findByName").returns(new Promise<RobotType>((resolve, reject) => {
+      resolve(RobotType.create(
+        {
+          "name": RobotTypeName.create(req.body.robotType).getValue(),
+          "taskTypes": [TaskType.delivery],
+          "robotTypeModel": RobotTypeModel.create("RB13").getValue(),
+          "brand": RobotTypeBrand.create("RB").getValue()
+        }, new UniqueEntityID("123")).getValue());
+    }));
+
+    let robotRepoInstance = Container.get("RobotRepo");
+    sinon.stub(robotRepoInstance, "save").returns(new Promise<Robot>((resolve, reject) => {
+      resolve(Robot.create(      {
+          "nickName": RobotNickName.create(req.body.nickName).getValue(),
+          "robotCode": RobotCode.create(req.body.robotCode).getValue(),
+          "serialNr": RobotSerialNr.create(req.body.serialNr).getValue(),
+          "description": RobotDescription.create(req.body.description).getValue(),
+          "state": true,
+          "robotType": RobotType.create({
+            "name": RobotTypeName.create(req.body.robotType).getValue(),
+            "taskTypes": [TaskType.delivery],
+            "robotTypeModel": RobotTypeModel.create("RB13").getValue(),
+            "brand": RobotTypeBrand.create("RB").getValue()
+          }, new UniqueEntityID("123")).getValue()
+        }
+        , new UniqueEntityID("123")).getValue());
+    }));
+
+    let robotServiceInstance = Container.get("RobotService");
+
+    const ctrl = new RobotController(robotServiceInstance as IRobotService);
+
+    // Act
+    await ctrl.createRobot(<Request>req, <Response>res, <NextFunction>next);
+
+    // Assert
+    sinon.assert.calledOnce(res.json);
+    sinon.assert.calledWith(res.json, sinon.match({
+        "state": "true",
+        "nickName": req.body.nickName,
+        "robotCode": req.body.robotCode,
+        "serialNr": req.body.serialNr,
+        "description": req.body.description,
+        "robotType": req.body.robotType
+      }
+    ));
+    // something wrong on the res.json on the controller
+  });
+
+
 });
