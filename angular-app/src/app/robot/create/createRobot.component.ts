9dada7bb41162c5c632e9e427312a6900ef23179
Delcio Monjane <1211739@isep.ipp.pt>
2023-11-12 20:47:04 +0000

    FRG-51 fixing error handle

--- angular-app/src/app/robot/create/createRobot.component.ts
+++ angular-app/src/app/robot/create/createRobot.component.ts
@@ -1,46 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Robot } from "../robot";
 import { RobotTypeService } from "../../robot-type/robot-type.service";
-import { CreateRobotService } from "./create-robot.service";
-import { Location } from '@angular/common';
+import { RobotService } from "../robot.service";
+import { Location } from "@angular/common";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './createRobot.component.html',
-  styleUrls: ['./createRobot.component.css']
+  selector: "app-create",
+  templateUrl: "./createRobot.component.html",
+  styleUrls: ["./createRobot.component.css"]
 })
-export class CreateRobotComponent implements OnInit{
-  constructor(private robotTypeService: RobotTypeService, private location: Location, private createRobotService: CreateRobotService) {
+export class CreateRobotComponent implements OnInit {
+  constructor(private robotTypeService: RobotTypeService, private location: Location, private createRobotService: RobotService) {
 
   }
-
 
   robot = new Robot();
   robotTypes: any;
 
   createRobot() {
-    this.createRobotService.createRobot(this.robot).subscribe(data => {
-      window.alert("Robot created successfully \n"
-                  + "Nickname: " + data.nickName + "\n"
-                  + "Robot type: " + data.robotType + "\n"
-                  + "Description: " + data.description + "\n"
-                  + "Robot Code: " + data.robotCode + "\n"
-                  + "Serial number: " + data.serialNr + "\n" );
+    this.createRobotService.createRobot(this.robot).subscribe({
+      next: (data) => {
+        window.alert("Robot created successfully \n"
+          + "Nickname: " + data.nickName + "\n"
+          + "Robot Type: " + data.robotType + "\n"
+          + "Description: " + data.description + "\n"
+          + "Robot code: " + data.robotCode + "\n"
+          + "Robot serial number: " + data.serialNr + "\n");
 
-      this.location.back();
-    }, error => {
-      window.alert("Error! Robot not created" + "\n" + JSON.stringify(error.error));
-      this.location.back();
+        this.location.back();
+      },
+      error: (error) => {
+        window.alert(JSON.stringify(error.error.error));
+      }
     });
   }
 
   ngOnInit() {
-     this.robotTypeService.getRobotTypes().subscribe(data => {
-       this.robotTypes = data;
-     });
+    this.robotTypeService.getRobotTypes().subscribe({
+      next: (data) => {
+        this.robotTypes = data;
+      },
+      error: (error) => {
+        window.alert(JSON.stringify(error.error.error));
+      }
+    });
   }
 
   getRobotTypeInfo(robotType: any) {
-    return "TasksTypes: " + robotType.taskTypes ;
+    return "TasksTypes: " + robotType.taskTypes;
   }
 }
