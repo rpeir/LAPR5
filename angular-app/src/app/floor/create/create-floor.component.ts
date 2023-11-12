737d6bb1e12cb3cf0571b1c92918e28df78a9c6c
Delcio Monjane <1211739@isep.ipp.pt>
2023-11-12 20:45:53 +0000

    FRG-29 fixing error handle

--- angular-app/src/app/floor/create/create-floor.component.ts
+++ angular-app/src/app/floor/create/create-floor.component.ts
@@ -6,35 +6,44 @@ import { Location } from "@angular/common";
 import { Building } from "../../building/building";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create-floor.component.html',
-  styleUrls: ['./create-floor.component.css']
+  selector: "app-create",
+  templateUrl: "./create-floor.component.html",
+  styleUrls: ["./create-floor.component.css"]
 })
-export class CreateFloorComponent implements OnInit{
+export class CreateFloorComponent implements OnInit {
 
   constructor(private createFloorService: FloorService, private location: Location, private buildingService: BuildingService) {
   }
 
   floor = new Floor();
   buildings: Building[] | undefined;
-  createFloor() {
-    this.createFloorService.createFloor(this.floor).subscribe(data => {
-      window.alert("Floor created successfully \n"
-                  + "Floor number: " + data.floorNr + "\n"
-                  + "Building: " + data.building + "\n"
-                  + "Description: " + data.description + "\n");
 
-      this.location.back();
-    }, error => {
-      window.alert("Error! Floor not created" + "\n" + JSON.stringify(error.error))
-      this.location.back();
+  createFloor() {
+    this.createFloorService.createFloor(this.floor).subscribe({
+      next: (data) => {
+        window.alert("Floor created successfully \n"
+          + "Floor number: " + data.floorNr + "\n"
+          + "Building: " + data.building + "\n"
+          + "Description: " + data.description + "\n");
+
+        this.location.back();
+      },
+      error: (error) => {
+        window.alert(JSON.stringify(error.error.error));
+      }
     });
   }
 
   ngOnInit() {
-    this.buildingService.getBuildings().subscribe(data => {
-      this.buildings = data;
-    });
+    this.buildingService.getBuildings().subscribe({
+        next: (data) => {
+          this.buildings = data;
+        },
+        error: (error) => {
+          window.alert(JSON.stringify(error.error.error));
+        }
+      }
+    );
   }
 
 }
