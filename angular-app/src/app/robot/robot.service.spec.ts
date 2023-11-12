cf8f0e4a4e89f63f3d4764a21294da37aeb379af
Delcio Monjane <1211739@isep.ipp.pt>
2023-11-12 20:48:11 +0000

    FRG-34 fixing error handle

--- angular-app/src/app/robot/robot.service.spec.ts
+++ angular-app/src/app/robot/robot.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CreateRobotService } from './create-robot.service';
+import { RobotService } from './robot.service';
 
 describe('CreateRobotService', () => {
-  let service: CreateRobotService;
+  let service: RobotService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(CreateRobotService);
+    service = TestBed.inject(RobotService);
   });
 
   it('should be created', () => {
