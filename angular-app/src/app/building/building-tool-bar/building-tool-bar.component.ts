1decf58f718eee8235364458a7d5fabd8c0a81eb
rpeir <1211345@isep.ipp.pt>
2023-11-16 00:33:31 +0000

    #FRG-25 #FRG-26 #FRG-27 #FRG-29 Corrected to new gestor-de-campus-toolbar and corrected the building-toolbar to sidebar

--- angular-app/src/app/building/building-tool-bar/building-tool-bar.component.ts
+++ angular-app/src/app/building/building-tool-bar/building-tool-bar.component.ts
@@ -7,7 +7,28 @@ import { Router } from "@angular/router";
   styleUrls: ['./building-tool-bar.component.css']
 })
 export class BuildingToolBarComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      switch (this.router.url) {
+        case "/building/create" :
+          // @ts-ignore
+          document.getElementById("create-building-option").style.fontWeight = "bold";
+          break;
+        case "/building/edit" :
+          // @ts-ignore
+          document.getElementById("edit-building-option").style.fontWeight = "bold";
+          break;
+        case "/building/list-all" :
+          // @ts-ignore
+          document.getElementById("list-all-buildings-option").style.fontWeight = "bold";
+          break;
+        case "/building/list-min-max" :
+          // @ts-ignore
+          document.getElementById("list-min-max-buildings-option").style.fontWeight = "bold";
+          break;
+      }
+    });
+  }
 
   goToCreateBuilding() {
     this.router.navigate(['/building/create']).then(r => console.log(r));
@@ -25,7 +46,4 @@ export class BuildingToolBarComponent {
     this.router.navigate(['']).then(r => console.log(r));
   }
 
-  goToGestorDeCampus() {
-    this.router.navigate(['/gestor-de-campus']);
-  }
 }
