d68c0861effe466ae4c49d6359c47f46245ec083
Ines <1210814@isep.ipp.pt>
2023-11-16 15:50:28 +0000

    FRG-70 Corrected robot animations and render circumstances.

--- angular-app/src/app/map-viewer/map-viewer.component.ts
+++ angular-app/src/app/map-viewer/map-viewer.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-map-viewer',
-  templateUrl: './map-viewer.component.html',
-  styleUrls: ['./map-viewer.component.css']
+  template: `
+    <button (click)="redirectToExternalPage()">Redirect</button>
+  `,
 })
 export class MapViewerComponent {
-
+  redirectToExternalPage() {
+    window.location.href = 'http://127.0.0.1:5500/angular-app/src/assets/map-renderer/Thumb_Raiser.html';
+  }
 }
--- angular-app/src/assets/map-renderer/animations_template.js
+++ angular-app/src/assets/map-renderer/animations_template.js
@@ -1,61 +1,55 @@
-import * as THREE from "three";
+import * as THREE from 'three';
 
 export default class Animations {
-    constructor(object, animations) {
-      /*
-        this.states = ["Idle", "Walking", "Running", "Dance", "Death", "Sitting", "Standing"];
-        this.emotes = ["Jump", "Yes", "No", "Wave", "Punch", "ThumbsUp"];
+  constructor(object, animations) {
+    this.states = ['animation.robot-runner.idle', 'animation.robot-runner.walk', 'animation.robot-runner.death'];
 
-        this.mixer = new THREE.AnimationMixer(object);
-        this.actionInProgress = false;
+    this.mixer = new THREE.AnimationMixer(object);
+    this.actionInProgress = false;
 
-        this.actions = {};
-        for (let i = 0; i < animations.length; i++) {
-            const clip = animations[i];
-            const action = this.mixer.clipAction(clip);
-            this.actions[clip.name] = action;
-            if (this.states.indexOf(clip.name) >= 4 || this.emotes.indexOf(clip.name) >= 0) {
-                action.clampWhenFinished = true;
-                action.loop = THREE.LoopOnce;
-            }
-        }
-        this.activeName = "Idle";
-        this.actions[this.activeName].play();
-       */
+    this.actions = {};
+    for (let i = 0; i < animations.length; i++) {
+      const clip = animations[i];
+      const action = this.mixer.clipAction(clip);
+      this.actions[clip.name] = action;
+      if (this.states.indexOf(clip.name) === 2) {
+        action.clampWhenFinished = true;
+        action.loop = THREE.LoopOnce;
+      }
     }
+    this.activeName = 'animation.robot-runner.idle';
+    this.actions[this.activeName].play();
+  }
 
-    fadeToAction(name, duration) {
-      /*
-        if (this.activeName != name && !this.actionInProgress) {
-            const previousName = this.activeName;
-            this.activeName = name;
-            this.actions[previousName].fadeOut(duration);
-            this.actions[this.activeName]
-                .reset()
-                .setEffectiveTimeScale(1)
-                .setEffectiveWeight(1)
-                .fadeIn(duration)
-                .play();
-            // Some actions must not be interrupted
-            if (this.activeName != "Idle" && this.activeName != "Walking" && this.activeName != "Running") {
-                this.mixer.addEventListener("finished", event => this.actionFinished(event));
-                this.actionInProgress = true;
-            }
-        }
-
-       */
+  fadeToAction(name, duration) {
+    if (this.activeName !== name && !this.actionInProgress) {
+      const previousName = this.activeName;
+      this.activeName = name;
+      this.actions[previousName].fadeOut(duration);
+      this.actions[this.activeName]
+        .reset()
+        .setEffectiveTimeScale(1)
+        .setEffectiveWeight(1)
+        .fadeIn(duration)
+        .play();
+      // Some actions must not be interrupted
+      if (this.activeName !== 'animation.robot-runner.idle' && this.activeName !== 'animation.robot-runner.walk') {
+        this.mixer.addEventListener('finished', event => this.actionFinished(event));
+        this.actionInProgress = true;
+      }
     }
+  }
 
-    actionFinished() {
-        if (this.actionInProgress) {
-            this.actionInProgress = false;
-            this.mixer.removeEventListener("finished", this.actionInProgress);
-        }
+  actionFinished() {
+    if (this.actionInProgress) {
+      this.actionInProgress = false;
+      this.mixer.removeEventListener('finished', this.actionInProgress);
     }
+  }
 
-    update(deltaT) {
-        if (this.mixer) {
-            this.mixer.update(deltaT);
-        }
+  update(deltaT) {
+    if (this.mixer) {
+      this.mixer.update(deltaT);
     }
+  }
 }
