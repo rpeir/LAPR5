cd368f070048b1fff9b66c116802320de0061266
JoseBarbosa <1211359@isep.ipp.pt>
2023-11-21 16:36:10 +0000

    FRG-86 Added unit tests to angular components ( create.component.spec.ts (elevator), replace.component.spec.ts (elevator)

--- angular-app/src/app/elevator/create/create.component.spec.ts
+++ angular-app/src/app/elevator/create/create.component.spec.ts
@@ -1,21 +1,86 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { ElevatorCreateComponent } from './create.component';
+import { ElevatorService} from "../elevator.service";
+import { BuildingService} from "../../building/building.service";
+import {FloorService} from "../../floor/floor.service";
+import {HttpClientModule} from "@angular/common/http";
+import {Elevator} from "../elevator";
+import {of, throwError} from "rxjs";
+
 
 describe('CreateComponent', () => {
   let component: ElevatorCreateComponent;
-  let fixture: ComponentFixture<ElevatorCreateComponent>;
+  let elevatorservice: ElevatorService;
+  let buildingservice: BuildingService;
+  let floorService: FloorService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ElevatorCreateComponent]
-    });
-    fixture = TestBed.createComponent(ElevatorCreateComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+  beforeEach(async () => {
+      await TestBed.configureTestingModule({
+          imports:[HttpClientModule],
+          providers: [ElevatorService,BuildingService,FloorService]
+      })
+          .compileComponents();
+      elevatorservice = TestBed.inject(ElevatorService);
+      buildingservice = TestBed.inject(BuildingService);
+
+      floorService = TestBed.inject(FloorService);
+      component = new ElevatorCreateComponent(elevatorservice, floorService, buildingservice);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should handle successful elevator creation', ()=> {
+      const mockElevator: Elevator = {
+          id: '1',
+          designation: 'Elevator 1',
+          buildingDesignation: 'Building A',
+          floorsServed: [1, 2, 3],
+          brand: 'Brand',
+          modelE: 'Model',
+          serialNumber: '12345',
+          description: 'Elevator description',
+      }
+      spyOn(elevatorservice, 'createElevator').and.returnValue(of(mockElevator));
+      spyOn(window, 'alert');
+
+      // Act
+      component.createElevator();
+
+      // Assert
+      const expectedAlertMessage = new RegExp(
+          'Elevator created successfully\\s*' +
+          'Designation: Elevator 1\\s*' +
+          'Building Designation: Building A\\s*' +
+          'Floors Served: 1,2,3\\s*' +
+          'Brand: Brand\\s*' +
+          'Model: Model\\s*' +
+          'Serial Number: 12345\\s*' +
+          'Description: Elevator description\\s*'
+      );
+
+      const actualAlertMessage = (window.alert as jasmine.Spy).calls.mostRecent().args[0];
+
+      expect(elevatorservice.createElevator).toHaveBeenCalledWith(component.elevator);
+      expect(actualAlertMessage).toMatch(expectedAlertMessage)
+  });
+    it('should handle elevator creation error', () => {
+        // Arrange
+        const errorResponse = {
+            error: {
+                error: 'Elevator creation failed'
+            }
+        };
+
+        spyOn(elevatorservice, 'createElevator').and.returnValue(throwError(errorResponse));
+        spyOn(window, 'alert');
+
+        // Act
+        component.createElevator();
+
+        // Assert
+        expect(elevatorservice.createElevator).toHaveBeenCalledWith(component.elevator);
+        expect(window.alert).toHaveBeenCalledWith(JSON.stringify(errorResponse.error.error));
+    });
 });
--- angular-app/src/app/elevator/replace/replace.component.spec.ts
+++ angular-app/src/app/elevator/replace/replace.component.spec.ts
@@ -1,21 +1,84 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { ElevatorReplaceComponent } from './replace.component';
+import { ElevatorService} from "../elevator.service";
+import { BuildingService} from "../../building/building.service";
+import {FloorService} from "../../floor/floor.service";
+import {HttpClientModule} from "@angular/common/http";
+import {Elevator} from "../elevator";
+import {of, throwError} from "rxjs";
+
 
 describe('CreateComponent', () => {
   let component: ElevatorReplaceComponent;
-  let fixture: ComponentFixture<ElevatorReplaceComponent>;
+  let elevatorservice: ElevatorService;
+  let buildingservice: BuildingService;
+  let floorService: FloorService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ElevatorReplaceComponent]
-    });
-    fixture = TestBed.createComponent(ElevatorReplaceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports:[HttpClientModule],
+      providers: [ElevatorService,BuildingService,FloorService]
+    })
+      .compileComponents();
+    elevatorservice = TestBed.inject(ElevatorService);
+    buildingservice = TestBed.inject(BuildingService);
+    floorService = TestBed.inject(FloorService);
+
+    component = new ElevatorReplaceComponent(buildingservice,elevatorservice,floorService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should handle successful elevator replacement', ()=> {
+    const mockElevator: Elevator = {
+      id: '1',
+      designation: 'Elevator 1',
+      buildingDesignation: 'Building A',
+      floorsServed: [1, 2, 3],
+      brand: 'Brand',
+      modelE: 'Model',
+      serialNumber: '12345',
+      description: 'Elevator description',
+    }
+    spyOn(elevatorservice, 'replaceElevator').and.returnValue(of(mockElevator));
+    spyOn(window, 'alert');
+
+    // Act
+    component.replaceElevator();
+
+    // Assert
+    const expectedAlertMessage = new RegExp(
+      'Elevator replaced successfully\\s*' +
+      'Designation: Elevator 1\\s*' +
+      'Building Designation: Building A\\s*' +
+      'Floors Served: 1,2,3\\s*' +
+      'Brand: Brand\\s*' +
+      'Model: Model\\s*' +
+      'Serial Number: 12345\\s*' +
+      'Description: Elevator description\\s*'
+
+  );
+    const actualAlertMessage = (window.alert as jasmine.Spy).calls.mostRecent().args[0];
+
+    expect(actualAlertMessage).toMatch(expectedAlertMessage);
+    expect(elevatorservice.replaceElevator).toHaveBeenCalledWith(component.elevator);
+  });
+  it('should handle elevator replacement error', () => {
+    spyOn(elevatorservice, 'replaceElevator').and.returnValue(throwError({error:{error:'Error'}}));
+    spyOn(window, 'alert');
+
+    // Act
+    component.replaceElevator();
+
+    // Assert
+    const expectedAlertMessage = new RegExp(
+      'Error'
+    );
+    const actualAlertMessage = (window.alert as jasmine.Spy).calls.mostRecent().args[0];
+
+    expect(actualAlertMessage).toMatch(expectedAlertMessage);
+    expect(elevatorservice.replaceElevator).toHaveBeenCalledWith(component.elevator);
+  })
 });
