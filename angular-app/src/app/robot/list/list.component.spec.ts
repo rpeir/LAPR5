955ebb6571cf0b514481d0953d43ae114b16ee00
JoseBarbosa <1211359@isep.ipp.pt>
2023-11-21 18:33:04 +0000

    FRG-86 Added unit tests to angular components ( create.component.spec.ts (elevator), replace.component.spec.ts (elevator)

--- angular-app/src/app/robot/list/list.component.spec.ts
+++ angular-app/src/app/robot/list/list.component.spec.ts
@@ -1,21 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {Robot} from "../robot";
+import {of} from "rxjs";
+import {RobotService} from "../robot.service";
+
 
 describe('ListComponent', () => {
   let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
+  let robotService: RobotService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ListComponent]
-    });
-    fixture = TestBed.createComponent(ListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports:[HttpClientModule],
+      providers: [RobotService]
+    })
+      .compileComponents();
+    robotService = TestBed.inject(RobotService);
+
+
+    component = new ListComponent(robotService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should handle successful robot listing', ()=> {
+    const mockRobot: Robot = {
+      robotCode: '1',
+      nickName: 'Robot 1',
+      robotType: 'Delivery',
+      serialNr: '12345',
+      description: 'Robot description',
+      state: 'true'
+    }
+    spyOn(robotService, 'listAllRobots').and.returnValue(of([mockRobot]));
+
+    // Act
+    component.listRobots();
+
+    // Assert
+    expect(component.robots).toEqual([mockRobot]);
   });
 });
