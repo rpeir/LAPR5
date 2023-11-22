7b94b929b97020cc0b4e10051510b8e809959476
rpeir <1211345@isep.ipp.pt>
2023-11-22 12:03:33 +0000

    #FRG-65 Corrected createCollumn algorithm to new type of FloorGraph

--- prolog/algorithms2.pl
+++ prolog/algorithms2.pl
@@ -1,24 +1,78 @@
 :-dynamic connectCell/2.
 
-createGraph(_,0):-!.
+createGraph(_,-1):-!.
 createGraph(Line,Collum):-createCollum(Line,Collum),
           NewCollum is Collum - 1,
           createGraph(Line,NewCollum).
 
-createCollum(0,_):-!.
+% old
+%createCollum(0,_):-!.
+%createCollum(Line,Collum):-m(Line,Collum,0),!,
+%          NextLine is Line + 1, PreviousLine is Line - 1, NextCollum is Collum + 1, PreviousCollum is Collum - 1,
+%          ((m(NextLine, Collum,0), assertz(connectCell(cel(Line,Collum),cel(NextLine,Collum)));true)),
+%          ((m(PreviousLine, Collum,0), assertz(connectCell(cel(Line,Collum),cel(PreviousLine,Collum)));true)),
+%          ((m(Line, NextCollum,0), assertz(connectCell(cel(Line,Collum),cel(Line,NextCollum)));true)),
+%          ((m(Line, PreviousCollum,0), assertz(connectCell(cel(Line,Collum),cel(Line,PreviousCollum)));true)),
+%          %diagonals
+%          ((m(Line,NextCollum,0), m(NextLine,Collum,0), assertz(connectCell(cel(Line,Collum),cel(NextLine,NextCollum)));true)),
+%          ((m(Line,PreviousCollum,0), m(NextLine,Collum,0), assertz(connectCell(cel(Line,Collum),cel(NextLine,PreviousCollum)));true)),
+%          ((m(Line,NextCollum,0), m(PreviousLine,Collum,0), assertz(connectCell(cel(Line,Collum),cel(PreviousLine,NextCollum)));true)),
+%          ((m(Line,PreviousCollum,0), m(PreviousLine,Collum,0), assertz(connectCell(cel(Line,Collum),cel(PreviousLine,PreviousCollum)));true)),
+%          Line1 is Line -1,
+%          createCollum(Line1,Collum).
+%createCollum(Line,Collum):-Line1 is Line -1,
+%          createCollum(Line1,Collum).
+
+createCollum(-1,_):-!.
 createCollum(Line,Collum):-m(Line,Collum,0),!,
           NextLine is Line + 1, PreviousLine is Line - 1, NextCollum is Collum + 1, PreviousCollum is Collum - 1,
-          ((m(NextLine, Collum,0), assertz(connectCell(cel(Line,Collum),cel(NextLine,Collum)));true)),
-          ((m(PreviousLine, Collum,0), assertz(connectCell(cel(Line,Collum),cel(PreviousLine,Collum)));true)),
-          ((m(Line, NextCollum,0), assertz(connectCell(cel(Line,Collum),cel(Line,NextCollum)));true)),
-          ((m(Line, PreviousCollum,0), assertz(connectCell(cel(Line,Collum),cel(Line,PreviousCollum)));true)),
-          %diagonals
-          ((m(Line,NextCollum,0), m(NextLine,Collum,0), assertz(connectCell(cel(Line,Collum),cel(NextLine,NextCollum)));true)),
-          ((m(Line,PreviousCollum,0), m(NextLine,Collum,0), assertz(connectCell(cel(Line,Collum),cel(NextLine,PreviousCollum)));true)),
-          ((m(Line,NextCollum,0), m(PreviousLine,Collum,0), assertz(connectCell(cel(Line,Collum),cel(PreviousLine,NextCollum)));true)),
-          ((m(Line,PreviousCollum,0), m(PreviousLine,Collum,0), assertz(connectCell(cel(Line,Collum),cel(PreviousLine,PreviousCollum)));true)),
+          % pode ir para cima
+          (assertz(connectCell(cel(Line,Collum),cel(PreviousLine,Collum)));true),
+          % verifica se pode ir para baixo
+          ((m(NextLine, Collum,1);m(NextLine, Collum,0)), assertz(connectCell(cel(Line,Collum),cel(NextLine,Collum)));true),
+          % pode ir para a esquerda
+          (assertz(connectCell(cel(Line,Collum),cel(Line,PreviousCollum)));true),
+          % verifica se pode ir para a direita
+          ((m(Line, NextCollum,2);m(Line, NextCollum,0)), assertz(connectCell(cel(Line,Collum),cel(Line,NextCollum)));true),
           Line1 is Line -1,
           createCollum(Line1,Collum).
+
+createCollum(Line,Collum):-m(Line,Collum,1),!,
+          NextLine is Line + 1, PreviousLine is Line - 1, NextCollum is Collum + 1,
+          % pode ir para cima
+          (assertz(connectCell(cel(Line,Collum),cel(PreviousLine,Collum)));true),
+          % verifica se pode ir para baixo
+          ((m(NextLine, Collum,1);m(NextLine, Collum,0)), assertz(connectCell(cel(Line,Collum),cel(NextLine,Collum)));true),
+          % NAO pode ir para a esquerda, pq tem parede á esquerda
+          % verifica se pode ir para a direita
+          ((m(Line, NextCollum,2);m(Line, NextCollum,0)), assertz(connectCell(cel(Line,Collum),cel(Line,NextCollum)));true),
+          Line1 is Line -1,
+          createCollum(Line1,Collum).
+
+createCollum(Line,Collum):-m(Line,Collum,2),!,
+          NextLine is Line + 1, NextCollum is Collum + 1, PreviousCollum is Collum - 1,
+           % NAO pode ir para cima, pq tem parede em cima
+           % verifica se pode ir para baixo
+           ((m(NextLine, Collum,1);m(NextLine, Collum,0)), assertz(connectCell(cel(Line,Collum),cel(NextLine,Collum)));true),
+           % pode ir para a esquerda
+           (assertz(connectCell(cel(Line,Collum),cel(Line,PreviousCollum)));true),
+           % verifica se pode ir para a direita
+           ((m(Line, NextCollum,2);m(Line, NextCollum,0)), assertz(connectCell(cel(Line,Collum),cel(Line,NextCollum)));true),
+           Line1 is Line -1,
+           createCollum(Line1,Collum).
+
+createCollum(Line,Collum):-m(Line,Collum,3),!,
+          NextLine is Line + 1, NextCollum is Collum + 1,
+          % NAO pode ir para cima, pq tem parede em cima
+          % verifica se pode ir para baixo
+          ((m(NextLine, Collum,1);m(NextLine, Collum,0)), assertz(connectCell(cel(Line,Collum),cel(NextLine,Collum)));true),
+          % NAO pode ir para a esquerda, pq tem parede á esquerda
+          % verifica se pode ir para a direita
+          ((m(Line, NextCollum,2);m(Line, NextCollum,0)), assertz(connectCell(cel(Line,Collum),cel(Line,NextCollum)));true),
+          Line1 is Line -1,
+          createCollum(Line1,Collum).
+
+
 createCollum(Line,Collum):-Line1 is Line -1,
           createCollum(Line1,Collum).
 
