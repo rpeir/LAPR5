94390fa3eb3a65ddb040f065be13971747406e80
Ines <1210814@isep.ipp.pt>
2023-11-22 16:57:07 +0000

    FRG-27 - List All buildings UI implemented.

--- angular-app/src/app/building/list-all/list-all.component.ts
+++ angular-app/src/app/building/list-all/list-all.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { BuildingService } from '../building.service';
+import { Building } from '../building';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-list-all',
   templateUrl: './list-all.component.html',
-  styleUrls: ['./list-all.component.css']
+  styleUrls: ['./list-all.component.css'],
 })
 export class ListAllComponent {
+  constructor(private createBuildingService: BuildingService, private location: Location) {}
 
+  buildings: Building[] | any;
+
+  ngOnInit(): void {
+    this.getBuildings();
+  }
+
+  getBuildings() {
+    this.createBuildingService.listAllBuilding().subscribe({
+      next: data => {
+        this.buildings = data;
+      },
+      error: error => {
+        window.alert(JSON.stringify(error.error.error));
+      },
+    });
+  }
 }
