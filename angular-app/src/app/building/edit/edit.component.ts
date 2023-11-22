d171044b4f50e9a6fdf71bc53fba1d352026985f
Ines <1210814@isep.ipp.pt>
2023-11-22 22:29:44 +0000

    FRG-26 - Altered Edit Building UI to use dropdown to select building to edit.

--- angular-app/src/app/building/edit/edit.component.ts
+++ angular-app/src/app/building/edit/edit.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Building } from '../building';
 import { BuildingService } from '../building.service';
+import { MatSelectChange } from '@angular/material/select';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-edit',
@@ -9,11 +11,11 @@ import { BuildingService } from '../building.service';
   styleUrls: ['./edit.component.css'],
 })
 export class EditComponent implements OnInit {
-  buildings: Building[] | undefined;
-  building = new Building();
+  buildings: Building[] = [];
+  building: Building = new Building();
   editForm: FormGroup;
 
-  constructor(private buildingService: BuildingService, private fb: FormBuilder) {
+  constructor(private buildingService: BuildingService, private fb: FormBuilder, private cdr: ChangeDetectorRef) {
     this.editForm = this.fb.group({
       code: [''],
       designation: [''],
@@ -22,6 +24,35 @@ export class EditComponent implements OnInit {
       width: [''],
       height: [''],
     });
+  }
+
+  onBuildingCodeChange(event: MatSelectChange) {
+    const selectedCode = event.value;
+    // Find the building with the selected code
+    const selectedBuilding = this.buildings.find(b => b.code === selectedCode);
+    // Check if selectedBuilding is not undefined
+    if (selectedBuilding) {
+      if (this.editForm.dirty) {
+        this.editForm.reset();
+      }
+      // Set the form values
+      this.editForm.patchValue({
+        code: selectedBuilding.code,
+        designation: selectedBuilding.designation,
+        description: selectedBuilding.description,
+        length: selectedBuilding.length,
+        width: selectedBuilding.width,
+        height: selectedBuilding.height,
+      });
+      // Set the building
+      this.building = selectedBuilding;
+      // Mark the form as dirty
+      this.editForm.markAsDirty();
+      // Trigger change detection
+      this.cdr.detectChanges();
+    } else {
+      console.warn(`Building with code ${selectedCode} not found.`);
+    }
   }
 
   ngOnInit() {
@@ -36,12 +67,19 @@ export class EditComponent implements OnInit {
   }
 
   updateBuilding() {
-    this.buildingService.editBuilding(this.building).subscribe({
+    const buildingUpdated = new Building();
+    buildingUpdated.code = this.building.code;
+    buildingUpdated.designation = this.building.designation;
+    buildingUpdated.description = this.building.description;
+    buildingUpdated.length = this.building.length;
+    buildingUpdated.width = this.building.width;
+    buildingUpdated.height = this.building.height;
+    this.buildingService.editBuilding(buildingUpdated).subscribe({
       next: data => {
         window.alert(
           'Building updated successfully \n' +
             'Code: ' +
-            data.code +
+            (data.code ?? buildingUpdated.code) +
             '\n' +
             'Designation: ' +
             data.designation +
