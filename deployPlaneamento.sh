9d5270e4c938eee59aacca502add2c43cc261a2b
rpeir <1211345@isep.ipp.pt>
2023-11-24 23:59:44 +0000

    #FRG-59 Corrcted pipeline

--- deployPlaneamento.sh
+++ deployPlaneamento.sh
@@ -18,4 +18,3 @@ cd src/ || (echo "Error entering modulo_planeamento/src" && exit 1)
 swipl server.pl || (echo "Error starting server" && exit 1)
 
 echo "Deploy script finished execution successfully"
-exit 0
