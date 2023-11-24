119fbe06e979d33a27a7dad934ef04f090d83189
rpeir <1211345@isep.ipp.pt>
2023-11-24 16:06:28 +0000

    #FRG-59 Corrected pipeline of deploy in VM

--- deployPlaneamento.sh
+++ deployPlaneamento.sh
@@ -3,18 +3,15 @@
 echo "Deploy script started"
 
 # Atualilza o sistema
-(echo $sudopass | sudo -S apt-get update) || exit 1
-(echo $sudopass | sudo -S apt-get upgrade -y) || exit 1
-
 cd froggers_sem5pi/ || exit 1
 
 # Atualiza o repositorio
 git pull || exit 1
 
-cd modulo_planeamento/ || exit 1
+cd modulo_planeamento/ || (echo "Error entering modulo_planeamento/" && exit 1)
 
 swipl start.pl || exit 1
 
-echo "Deploy script finished execution"
+echo "Deploy script finished execution successfully"
 
 exit 0
