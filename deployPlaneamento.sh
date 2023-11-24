af92b1a00f3b4d6fea367674b2b168c1b742ff1f
rpeir <1211345@isep.ipp.pt>
2023-11-24 14:43:09 +0000

    #FRG-59 Corrected pipeline

--- deployPlaneamento.sh
+++ deployPlaneamento.sh
@@ -3,17 +3,17 @@
 echo "Deploy script started"
 
 # Atualilza o sistema
-(echo $sudopass | sudo -S apt-get update) | exit 1
-echo $sudopass | sudo -S apt-get upgrade -y | exit 1
+(echo $sudopass | sudo -S apt-get update) || exit 1
+(echo $sudopass | sudo -S apt-get upgrade -y) || exit 1
 
-cd froggers_sem5pi/ | exit 1
+cd froggers_sem5pi/ || exit 1
 
 # Atualiza o repositorio
-git pull | exit 1
+git pull || exit 1
 
-cd modulo_planeamento/ | exit 1
+cd modulo_planeamento/ || exit 1
 
-swipl start.pl | exit 1
+swipl start.pl || exit 1
 
 echo "Deploy script finished execution"
 
