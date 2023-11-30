24fe2f2ea192b0b47f0610b90136508aa3ad19f8
rpeir <1211345@isep.ipp.pt>
2023-11-30 11:28:27 +0000

    Corrected planning host ip

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ export default {
   /**
    * Host of planning server
    */
-  planningHost: process.env.PLANNING_HOST || 'localhost',
+  planningHost: process.env.PLANNING_HOST || 'http://vs1141.dei.isep.ipp.pt:5000',
 
   /**
    * API configs
