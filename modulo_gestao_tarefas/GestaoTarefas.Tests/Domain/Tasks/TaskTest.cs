ff7ecb94b2da5e3ff3d13d38b66804451d6d9bab
rpeir <1211345@isep.ipp.pt>
2023-12-15 14:25:49 +0000

    Changed modulo_gestao_tarefas folder structure to add Tests project

---
+++ modulo_gestao_tarefas/GestaoTarefas.Tests/Domain/Tasks/TaskTest.cs
@@ -0,0 +1,16 @@
+using GestaoTarefas.Domain.Tasks;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GestaoTarefas.Tests.Domain.Tasks;
+
+[TestClass]
+[TestSubject(typeof(Task))]
+public class TaskTest
+{
+
+  [TestMethod]
+  public void METHOD()
+  {
+  }
+}
