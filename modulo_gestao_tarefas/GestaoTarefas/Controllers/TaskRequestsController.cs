d908acef254bd730c2968709525a783b938298c8
Ines <1210814@isep.ipp.pt>
2024-01-02 15:33:47 +0000

    FRG-152 - Implemented filtering for requests based on date, user, robot Id and status.

--- modulo_gestao_tarefas/GestaoTarefas/Controllers/TaskRequestsController.cs
+++ modulo_gestao_tarefas/GestaoTarefas/Controllers/TaskRequestsController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using GestaoTarefas.Domain.Shared;
 using GestaoTarefas.Domain.TaskRequests;
+using GestaoTarefas.Domain.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SYSTask = System.Threading.Tasks;
 
@@ -12,10 +14,12 @@ namespace GestaoTarefas.Controllers;
 public class TaskRequestsController : ControllerBase
 {
   private readonly TaskRequestService _service;
+  private readonly TaskService _taskService;
 
-  public TaskRequestsController(TaskRequestService service)
+  public TaskRequestsController(TaskRequestService service, TaskService taskService)
   {
     _service = service;
+    _taskService = taskService;
   }
 
   // GET: api/TaskRequests
@@ -35,6 +39,26 @@ public class TaskRequestsController : ControllerBase
           statusDto: Request.Query["status"],
           userIdDto: Request.Query["userId"]
           );
+      }
+
+      // GET: api/TaskRequests?status=xyz&robotId=xyz,xyz,xyz&userId=xyz&startTime=time1&endTime=time2
+      if (Request.Query.ContainsKey("status") || Request.Query.ContainsKey("robotId") || Request.Query.ContainsKey("userId") || Request.Query.ContainsKey("startTime") || Request.Query.ContainsKey("endTime"))
+      {
+        var resultNoRobot = await this._service.GetByStatusUserTimeAsync(
+          statusDto: Request.Query["status"],
+          userIdDto: Request.Query["userId"],
+          startTimeDto: Request.Query["startTime"],
+          endTimeDto: Request.Query["endTime"]
+          );
+
+        if (!Request.Query.ContainsKey("robotId")) return resultNoRobot;
+        //split robots by comma
+        List<string> robots = Request.Query["robotId"].ToString().Split(',').ToList();
+        var tasksWithRobots = await this._taskService.GetByRobotsAsync(robots);
+
+        var taskRequestsWithTasksAssigned = await this._service.GetByTasksAsync(tasksWithRobots);
+
+        return await _service.FindRequestsWithAllFilters(resultNoRobot,taskRequestsWithTasksAssigned);
       }
 
       return StatusCode(400);
